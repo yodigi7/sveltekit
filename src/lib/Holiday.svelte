8273e293532a8fcd60b124ac142f5cdcf8466658
Anthony Buchholz <anthony.buchholz@gmail.com>
2023-09-24 00:13:11 -0500

    Extract out holidays

---
+++ src/lib/Holiday.svelte
@@ -0,0 +1,16 @@
+<script>
+    /**@type {any}*/
+    export let holiday;
+    let date = new Date(holiday.date.iso);
+</script>
+
+
+<a href={holiday.canonical_url}>{holiday.name}</a>
+<br>
+Date: {`${date.toDateString()}`}
+<br>
+Type: {holiday.primary_type}
+<br>
+{holiday.description}
+<br>
+<br>
